e706725c02b49c8b04a8cf3ce924f467e7ee152c
BenjaFA <b.fernandez07@ufromail.cl>
2023-10-15 17:31:01 -0300

    Preguntar por la distancia

--- src/test/java/AutoTest.java
+++ src/test/java/AutoTest.java
@@ -20,5 +20,13 @@ class AutoTest {
 
     @Test
     void calcularTiempoAuto() {
+        Auto auto = new Auto("Ejemplo");
+        double autoV = 20; //Variable que no se cambia
+        //VARIABLE A PROBRAR
+        double distancia = 100;         // Distancia en kilómetros
+        double tiempoEsperado = 5.0;    // Tiempo esperado en horas (para una velocidad de 20 km/h)
+        double tiempoCalculado = distancia/autoV;
+
+        assertEquals(tiempoEsperado, tiempoCalculado, 0.01); // Aceptamos un margen de error de 0.01 horas
     }
 }
